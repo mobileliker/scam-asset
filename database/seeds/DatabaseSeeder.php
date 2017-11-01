e3ebd8e1a799feade196f80cc30c35f454224ac7
wuzhihui819 <wuzhihui819@163.com>
2017-11-01 16:33:35 +0800

    [FIX]修复生产环境无法正确执行InitSeeder的错误

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -2,6 +2,14 @@
 
 use Illuminate\Database\Seeder;
 
+/**
+ * 基础填充器
+ * @version : 2.0
+ * @author : wuzhihui
+ * @date : 2017/11/1
+ * @description : 
+ * (1)修复生产环境无法正确执行InitSeeder的错误；（2017/11/1）
+
 class DatabaseSeeder extends Seeder
 {
     /**
